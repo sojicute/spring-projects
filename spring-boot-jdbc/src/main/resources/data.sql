bd27a45508f31c91915f06af0ac5e1a7be4a0022
unknown <unknown@internet>
2021-10-06 17:00:07 +0400

    Updated data.sql

--- spring-boot-jdbc/src/main/resources/data.sql
+++ spring-boot-jdbc/src/main/resources/data.sql
@@ -1,5 +1,18 @@
-INSERT INTO GENRES (id, name) VALUES (1, 'GENRE NAME');
-INSERT INTO AUTHORS (id, name) VALUES (1, 'AUTHOR NAME');
-INSERT INTO BOOKS (id, title, genre_id, author_id) VALUES (1, 'BOOK 1', 1, 1);
-INSERT INTO BOOKS (id, title, genre_id, author_id) VALUES (2, 'BOOK 2', 1, 1);
-INSERT INTO BOOKS (id, title, genre_id, author_id) VALUES (3, 'BOOK 3', 1, 1);
+INSERT INTO GENRES (id, name) VALUES 
+	(1, 'Novel'),
+	(2, 'Science Fiction'),
+	(3, 'Horror'),
+	(4, 'Tragedy');
+
+INSERT INTO AUTHORS (id, name) VALUES 
+	(1, 'Leo Tolstoy'),
+	(2, 'F. Scott Fitzgerald'),
+	(3, 'Herman Melville'),
+	(4, 'William Shakespeare');
+
+INSERT INTO BOOKS (id, title, genre_id, author_id) VALUES 
+	(1, 'War and Peace', 1, 1),
+	(2, 'The Great Gatsby', 1, 2),
+	(3, 'Moby Dick', 1, 3),
+	(4, 'Hamlet', 4, 4),
+	(5, 'Anna Karenina', 1, 1);
